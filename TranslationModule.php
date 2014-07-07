3b6fc09c515b0de9e50b798a3586784a8573da1e
Lucas Bartholemy <lucas@bartholemy.com>
2014-07-07 15:38:30 +0200

    API Updates

--- TranslationModule.php
+++ TranslationModule.php
@@ -1,6 +1,7 @@
 <?php
 
-class TranslationModule extends HWebModule {
+class TranslationModule extends HWebModule
+{
 
     /**
      * On AdminNavigationWidget init, this callback will be called
@@ -10,17 +11,17 @@ class TranslationModule extends HWebModule {
      * 
      * @param type $event
      */
-    public static function onAdminMenuInit($event) {
+    public static function onAdminMenuInit($event)
+    {
         $event->sender->addItem(array(
-            'label' => Yii::t('TranslationModule.base', 'Translation'),
+            'label' => Yii::t('TranslationModule.base', 'Translation Manager'),
             'url' => Yii::app()->createUrl('//translation/translate'),
-            'icon' => '<i class="icon-user"></i>',
+            'icon' => '<i class="fa fa-align-left"></i>',
             'group' => 'manage',
             'sortOrder' => 1000,
             'isActive' => (Yii::app()->controller->module && Yii::app()->controller->module->id == 'translation' && Yii::app()->controller->id == 'translate'),
             'newItemCount' => 0
         ));
-        
     }
-    
+
 }
