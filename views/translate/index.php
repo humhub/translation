9cf36298d0b6c40ca31e0d57a1c734f246bc4911
Carlos Ferreira <c.ferreira@acsiiweb.com>
2017-05-03 14:12:37 +0200

    Small fix

--- views/translate/index.php
+++ views/translate/index.php
@@ -60,8 +60,8 @@ $bundle = MainAsset::register($this);
 
                     <?php $i = 0; ?>
                     <p>
-                        <?= Yii::t('TranslationModule.views_translate_index', 'If the value is empty, the message is considered as not translated.'); ?>
-                        <?= Yii::t('TranslationModule.views_translate_index', 'Messages that no longer need translation will have their translations enclosed between a pair of "@@" marks.'); ?>
+                        <?= Yii::t('TranslationModule.views_translate_index', 'If the value is empty, the message is considered as not translated.'); ?><br>
+                        <?= Yii::t('TranslationModule.views_translate_index', 'Messages that no longer need translation will have their translations enclosed between a pair of "@@" marks.'); ?><br>
                         <?= Yii::t('TranslationModule.views_translate_index', 'Message string can be used with plural forms format. Check i18n section of the documentation for details.'); ?>
                         <br/>
                         <br/>
