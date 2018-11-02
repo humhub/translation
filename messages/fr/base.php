d19dc63bcdee46e98cd8cc3bacbf617e4f2fe0d6
HumHub Translations <info@humhub.com>
2018-11-02 13:35:43 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/fr/base.php
+++ messages/fr/base.php
@@ -1,10 +1,9 @@
 <?php
-
-return [
-    'Could not save empty translation.' => '',
-    'Invalid translation pattern detected, please see {link}' => '',
-    'The translation contains an invalid parameter {match}' => '',
-    'Your input has been purified from suspicious html.' => '',
-    'Your translation seems to be empty and therefore could not be saved.' => '',
-    'Translations' => 'Traductions',
-];
+return array (
+  'Could not save empty translation.' => 'Impossible d\'enregistrer une traduction vide.',
+  'Invalid translation pattern detected, please see {link}' => 'Détection d\'un motif de traduction invalide, merci de lire {link}',
+  'The translation contains an invalid parameter {match}' => 'Cette traduction contient un paramètre invalide : {match}',
+  'Translations' => 'Traductions',
+  'Your input has been purified from suspicious html.' => 'Votre contribution a été nettoyée des éléments html suspicieux.',
+  'Your translation seems to be empty and therefore could not be saved.' => 'Votre traduction semble être vide, et n\'a donc pu être enregistrée.',
+);
--- messages/ja/base.php
+++ messages/ja/base.php
@@ -1,10 +1,9 @@
 <?php
-
-return [
-    'Could not save empty translation.' => '',
-    'Invalid translation pattern detected, please see {link}' => '',
-    'The translation contains an invalid parameter {match}' => '',
-    'Your input has been purified from suspicious html.' => '',
-    'Your translation seems to be empty and therefore could not be saved.' => '',
-    'Translations' => '翻訳',
-];
+return array (
+  'Could not save empty translation.' => '空の翻訳は保存できません',
+  'Invalid translation pattern detected, please see {link}' => '',
+  'The translation contains an invalid parameter {match}' => '',
+  'Translations' => '翻訳',
+  'Your input has been purified from suspicious html.' => '',
+  'Your translation seems to be empty and therefore could not be saved.' => '',
+);
