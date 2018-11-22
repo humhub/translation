f12858d874d7c52193884e9d13cf383b41b0be70
HumHub Translations <info@humhub.com>
2018-11-22 13:48:36 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/hu/base.php
+++ messages/hu/base.php
@@ -1,10 +1,9 @@
 <?php
-
-return [
-    'Could not save empty translation.' => '',
-    'Invalid translation pattern detected, please see {link}' => '',
-    'The translation contains an invalid parameter {match}' => '',
-    'Your input has been purified from suspicious html.' => '',
-    'Your translation seems to be empty and therefore could not be saved.' => '',
-    'Translations' => 'Fordítások',
-];
+return array (
+  'Could not save empty translation.' => 'Az üres fordításmezők nem kerültek mentésre.',
+  'Invalid translation pattern detected, please see {link}' => 'A program érvénytelen fordítási mintát észlelt, ld. {link}',
+  'The translation contains an invalid parameter {match}' => 'A fordítás érvénytelen paramétert tartalmaz {match}',
+  'Translations' => 'Fordítások',
+  'Your input has been purified from suspicious html.' => 'Az inputod gyanús html-ről lett kiszűrve.',
+  'Your translation seems to be empty and therefore could not be saved.' => 'Úgy tűnik, fordításod üres, ezért nem került mentésre.',
+);
