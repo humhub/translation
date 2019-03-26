cb91b2bd8cc51c71badb31d7319a590599059cdc
HumHub Translations <info@humhub.com>
2019-03-26 18:56:43 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/tr/base.php
+++ messages/tr/base.php
@@ -1,10 +1,9 @@
 <?php
-
-return [
-    'Could not save empty translation.' => '',
-    'Invalid translation pattern detected, please see {link}' => '',
-    'The translation contains an invalid parameter {match}' => '',
-    'Your input has been purified from suspicious html.' => '',
-    'Your translation seems to be empty and therefore could not be saved.' => '',
-    'Translations' => 'Çeviriler',
-];
+return array (
+  'Could not save empty translation.' => 'Boş çeviri kaydedilemedi.',
+  'Invalid translation pattern detected, please see {link}' => 'Geçersiz çeviri kalıbı algılandı, lütfen {link} bakın',
+  'The translation contains an invalid parameter {match}' => 'Çeviri geçersiz bir parametre {match} içeriyor',
+  'Translations' => 'Çeviriler',
+  'Your input has been purified from suspicious html.' => 'Girişiniz şüpheli html\'den temizlendi.',
+  'Your translation seems to be empty and therefore could not be saved.' => 'Çeviriniz boş görünüyor ve bu nedenle kaydedilemedi.',
+);
