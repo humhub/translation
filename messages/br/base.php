2a643c5fd4dc85a86de38214a5606f42263783d9
HumHub Translations <info@humhub.com>
2019-03-26 19:04:13 +0100

    Enh: Updated Translations (translate.humhub.org)

---
+++ messages/br/base.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'Could not save empty translation.' => '',
+    'Invalid translation pattern detected, please see {link}' => '',
+    'The translation contains an invalid parameter {match}' => '',
+    'Translations' => '',
+    'Your input has been purified from suspicious html.' => '',
+    'Your translation seems to be empty and therefore could not be saved.' => '',
+];
