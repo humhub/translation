2f1e9f082b32c74fd1273dab3f88fe8730e2a826
HumHub Translations <info@humhub.com>
2019-05-23 20:26:14 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/pl/base.php
+++ messages/pl/base.php
@@ -1,10 +1,9 @@
 <?php
-
-return [
-    'Could not save empty translation.' => '',
-    'Invalid translation pattern detected, please see {link}' => '',
-    'The translation contains an invalid parameter {match}' => '',
-    'Your input has been purified from suspicious html.' => '',
-    'Your translation seems to be empty and therefore could not be saved.' => '',
-    'Translations' => 'Tłumaczenia ',
-];
+return array (
+  'Could not save empty translation.' => 'Nie można zapisać pustego tłumaczenia.',
+  'Invalid translation pattern detected, please see {link}' => 'Wykryto niepoprawną składnie, zobacz {link}',
+  'The translation contains an invalid parameter {match}' => 'Tłumaczenie zawiera nieprawidłowy parametr {match}',
+  'Translations' => 'Tłumaczenia',
+  'Your input has been purified from suspicious html.' => 'Twoje dane zostały oczyszczone z podejrzanego kodu html.',
+  'Your translation seems to be empty and therefore could not be saved.' => 'Tłumaczenie wygląda na puste, więc nie zostało zapisane.',
+);
