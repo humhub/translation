24eaf1ae80847382dadcb418d42f55cf7b4ae2e8
HumHub Translations <info@humhub.com>
2020-03-04 14:37:27 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/de/views_translate_index.php
+++ messages/de/views_translate_index.php
@@ -1,17 +1,17 @@
 <?php
 return array (
   'File' => 'Datei',
-  'For more informations about translation syntax see' => '',
-  'If the value is empty, the message is considered as not translated.' => '',
+  'For more informations about translation syntax see' => 'Weitere Informationen zur Übersetzungssyntax finden sie unter',
+  'If the value is empty, the message is considered as not translated.' => 'Falls der Wert leer ist, gilt dieser Eintrag als "nicht übersetzt".',
   'Language' => 'Sprache',
   'Message string can be used with plural forms format. Check i18n section of the documentation for details.' => '',
   'Messages that no longer need translation will have their translations enclosed between a pair of "@@" marks.' => '',
   'Module' => 'Modul',
-  'Original (en-US)' => '',
+  'Original (en-US)' => 'Original (en-US)',
   'Save' => 'Speichern',
-  'Save before change!' => '',
-  'Saved!' => '',
+  'Save before change!' => 'Vor dem Ändern speichern!',
+  'Saved!' => 'Gespeichert!',
   'Search' => 'Suche',
-  'Translated' => '',
+  'Translated' => 'Übersetzt',
   'Translation Editor' => 'Übersetzungseditor',
 );
