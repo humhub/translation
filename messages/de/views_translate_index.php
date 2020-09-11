677e24073d57c7a75ca0a8639142c3c15fdb82d8
HumHub Translations <info@humhub.com>
2020-09-11 18:02:51 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/de/views_translate_index.php
+++ messages/de/views_translate_index.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    'For more informations about translation syntax see' => 'Weitere Informationen zur Übersetzungssyntax finden sie unter',
-    'If the value is empty, the message is considered as not translated.' => 'Falls der Wert leer ist, gilt dieser Eintrag als "nicht übersetzt".',
-    'Message string can be used with plural forms format. Check i18n section of the documentation for details.' => 'Der Nachrichtenstring kann im Pluralform-Format verwendet werden. Einzelheiten findest Du im Abschnitt i18n der Dokumentation.',
-    'Messages that no longer need translation will have their translations enclosed between a pair of "@@" marks.' => 'Bei Nachrichten, die nicht mehr übersetzt werden müssen, werden die Übersetzungen zwischen einem Paar "@@"-Marken eingeschlossen.',
-    'Original (en-US)' => 'Original (en-US)',
-    'Search' => 'Suche',
-    'Translated' => 'Übersetzt',
-    'Translation Editor' => 'Übersetzungseditor',
-    '<strong>Translation</strong> Editor' => '',
-];
+return array (
+  '<strong>Translation</strong> Editor' => '<strong>Übersetzungs</strong>-Editor',
+  'For more informations about translation syntax see' => 'Weitere Informationen zur Übersetzungssyntax finden sie unter',
+  'If the value is empty, the message is considered as not translated.' => 'Falls der Wert leer ist, gilt dieser Eintrag als "nicht übersetzt".',
+  'Message string can be used with plural forms format. Check i18n section of the documentation for details.' => 'Der Nachrichtenstring kann im Pluralform-Format verwendet werden. Einzelheiten findest Du im Abschnitt i18n der Dokumentation.',
+  'Messages that no longer need translation will have their translations enclosed between a pair of "@@" marks.' => 'Bei Nachrichten, die nicht mehr übersetzt werden müssen, werden die Übersetzungen zwischen einem Paar "@@"-Marken eingeschlossen.',
+  'Original (en-US)' => 'Original (en-US)',
+  'Search' => 'Suche',
+  'Translated' => 'Übersetzt',
+  'Translation Editor' => 'Übersetzungseditor',
+);
