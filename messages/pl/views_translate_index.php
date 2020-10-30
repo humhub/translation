36e6a17eb986208353a5322e687f182a40ac116e
HumHub Translations <info@humhub.com>
2020-10-30 18:04:44 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/pl/views_translate_index.php
+++ messages/pl/views_translate_index.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    'For more informations about translation syntax see' => 'Po więcej informacji dotyczących składni tłumaczeń zobacz',
-    'If the value is empty, the message is considered as not translated.' => 'Jeżeli wartość jest pusta, wiadomość jest uznawana za nieprzetłumaczoną.',
-    'Message string can be used with plural forms format. Check i18n section of the documentation for details.' => 'Wiadomość może być użyta w formie mnogiej. Sprawdź sekcje i18n dokumentacji po szczegóły.',
-    'Messages that no longer need translation will have their translations enclosed between a pair of "@@" marks.' => 'Wiadomości które już nie potrzebują tłumaczeń będą oznaczone pomiędzy parą "@@"',
-    'Original (en-US)' => 'Oryginalnie (en-US)',
-    'Search' => 'Szukaj',
-    'Translated' => 'Przetłumaczone',
-    'Translation Editor' => 'Edytor tłumaczeń',
-    '<strong>Translation</strong> Editor' => '',
-];
+return array (
+  '<strong>Translation</strong> Editor' => 'Edytor <strong>Tłumaczeń</strong>',
+  'For more informations about translation syntax see' => 'Po więcej informacji dotyczących składni tłumaczeń zobacz',
+  'If the value is empty, the message is considered as not translated.' => 'Jeżeli wartość jest pusta, wiadomość jest uznawana za nieprzetłumaczoną.',
+  'Message string can be used with plural forms format. Check i18n section of the documentation for details.' => 'Wiadomość może być użyta w formie mnogiej. Sprawdź sekcje i18n dokumentacji po szczegóły.',
+  'Messages that no longer need translation will have their translations enclosed between a pair of "@@" marks.' => 'Wiadomości które już nie potrzebują tłumaczeń będą oznaczone pomiędzy parą "@@"',
+  'Original (en-US)' => 'Oryginalnie (en-US)',
+  'Search' => 'Szukaj',
+  'Translated' => 'Przetłumaczone',
+  'Translation Editor' => 'Edytor tłumaczeń',
+);
