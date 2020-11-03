ec729fd8ccc8f9973d731fe49a9d9df93e309b9f
HumHub Translations <info@humhub.com>
2020-11-03 11:03:48 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/bg/views_translate_index.php
+++ messages/bg/views_translate_index.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    'Search' => 'Търси',
-    '<strong>Translation</strong> Editor' => '',
-    'For more informations about translation syntax see' => '',
-    'If the value is empty, the message is considered as not translated.' => '',
-    'Message string can be used with plural forms format. Check i18n section of the documentation for details.' => '',
-    'Messages that no longer need translation will have their translations enclosed between a pair of "@@" marks.' => '',
-    'Original (en-US)' => '',
-    'Translated' => '',
-    'Translation Editor' => '',
-];
+return array (
+  '<strong>Translation</strong> Editor' => 'Редактор за <strong>превод</strong>',
+  'For more informations about translation syntax see' => 'За повече информация относно синтаксиса на преводите погледнете',
+  'If the value is empty, the message is considered as not translated.' => 'Ако превода е празен, съобщението се счита за непреведено.',
+  'Message string can be used with plural forms format. Check i18n section of the documentation for details.' => 'Низът на съобщението може да се използва с формат за множествено число. Проверете раздела i18n от документацията за подробности.',
+  'Messages that no longer need translation will have their translations enclosed between a pair of "@@" marks.' => 'Съобщенията, които вече не се нуждаят от превод, ще имат преводи, затворени между двойка знаци "@@".',
+  'Original (en-US)' => 'Original (en-US)',
+  'Search' => 'Търсене',
+  'Translated' => 'Превод',
+  'Translation Editor' => 'Редактор за превод',
+);
