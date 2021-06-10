61c22ace733c24e1d92a90f29120ede703a63f82
HumHub Translations <info@humhub.com>
2021-06-10 20:14:48 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/th/views_translate_index.php
+++ messages/th/views_translate_index.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    'Search' => 'ค้นหา',
-    'Translation Editor' => 'ตัวแก้ไขการแปลภาษา',
-    '<strong>Translation</strong> Editor' => '',
-    'For more informations about translation syntax see' => '',
-    'If the value is empty, the message is considered as not translated.' => '',
-    'Message string can be used with plural forms format. Check i18n section of the documentation for details.' => '',
-    'Messages that no longer need translation will have their translations enclosed between a pair of "@@" marks.' => '',
-    'Original (en-US)' => '',
-    'Translated' => '',
-];
+return array (
+  '<strong>Translation</strong> Editor' => '<strong>การแปล</strong> บรรณาธิการ',
+  'For more informations about translation syntax see' => 'สำหรับข้อมูลเพิ่มเติมเกี่ยวกับรูปแบบการแปล โปรดดูที่',
+  'If the value is empty, the message is considered as not translated.' => 'หากค่าว่างเปล่า จะถือว่าข้อความไม่ได้แปล',
+  'Message string can be used with plural forms format. Check i18n section of the documentation for details.' => 'สตริงข้อความใช้กับรูปแบบพหูพจน์ได้ ตรวจสอบส่วน i18n ของเอกสารประกอบสำหรับรายละเอียด',
+  'Messages that no longer need translation will have their translations enclosed between a pair of "@@" marks.' => 'ข้อความที่ไม่ต้องการการแปลอีกต่อไปจะมีคำแปลอยู่ระหว่างเครื่องหมาย "@@"',
+  'Original (en-US)' => 'ต้นฉบับ (en-US)',
+  'Search' => 'ค้นหา',
+  'Translated' => 'แปลแล้ว',
+  'Translation Editor' => 'บรรณาธิการแปล',
+);
