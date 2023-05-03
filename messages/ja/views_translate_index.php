e50526de52bd95ae9623b0faa8fd7c90e13f2341
HumHub Translations <hello@humhub.com>
2023-05-03 17:00:17 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/ja/views_translate_index.php
+++ messages/ja/views_translate_index.php
@@ -1,9 +1,9 @@
 <?php
 return array (
   '<strong>Translation</strong> Editor' => '<strong>翻訳</strong> 編集',
-  'For more informations about translation syntax see' => '変換構文の詳細については、次を参照して下さい。',
+  'For more informations about translation syntax see' => '変換構文の詳細については、次を参照してください。',
   'If the value is empty, the message is considered as not translated.' => '値が空の場合、メッセージは翻訳されていないと見なされます。',
-  'Message string can be used with plural forms format. Check i18n section of the documentation for details.' => 'メッセージ文字列は複数形で使用できます。詳細については、ドキュメントの i18n セクションを確認して下さい。',
+  'Message string can be used with plural forms format. Check i18n section of the documentation for details.' => 'メッセージ文字列は複数形で使用できます。詳細については、ドキュメントの i18n セクションを確認してください。',
   'Messages that no longer need translation will have their translations enclosed between a pair of "@@" marks.' => '翻訳が不要になったメッセージは、翻訳が "@@" マークのペアで囲まれます。',
   'Original (en-US)' => 'オリジナル (en-US)',
   'Search' => '検索',
