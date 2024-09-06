18ba2d7a768de0000716d828b582c3583ecdc459
Yuriy Bakhtin <yurybakh@gmail.com>
2024-09-06 10:37:10 +0200

    Fix tests for case when module is not initialized

--- models/MessageFile.php
+++ models/MessageFile.php
@@ -109,7 +109,7 @@ class MessageFile extends TranslationPath
         $messages = dirname($languageDir);
         $moduleId = dirname($messages);
 
-        $expectedModuleId = $this->isCoreModulePath() ? 'humhub' : basename($this->module->getBasePath());
+        $expectedModuleId = $this->isCoreModulePath() ? 'humhub' : basename($this->getModule()->getBasePath());
 
         if(!($this->validateParent($languageDir, $language) || $this->validateParent($languageDir, static::toLegacyLanguageCode($language)))
             || !$this->validateParent($messages, 'messages')
